bfb2db268e4ee5ddad41350478a73fabaa3aec40
Samuel Kent <kent_sam@outlook.com>
2020-07-05 18:55:56 +0100

    Backend connection working.

--- product-scan.js
+++ product-scan.js
@@ -2,14 +2,14 @@
 
 "use strict";
 
-const BACK_END_URL = "http://recyclabilitydiscriminator.eu-gb.mybluemix.net/querythis"
+const BACK_END_URL = "https://recyclabilitydiscriminator.eu-gb.mybluemix.net/amazonproduct"
 
 // Checks for the ASIN on the Amazon page.
 function getASIN() {
     let tableRows = document.getElementsByTagName("td");
     let ASIN;
 
-    for(let i = 0; i < tableRows.length; i++){
+    for (let i = 0; i < tableRows.length; i++) {
         let node = tableRows[i];
         if (node.innerHTML == "ASIN") {
             let tableRow = node.parentElement.childNodes;
@@ -32,22 +32,26 @@ function getASIN() {
 
 async function postData(url, data) {
     let response = await fetch(url, {
-      method: 'POST',
-      mode: 'no-cors',
-      cache: 'no-cache',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      redirect: 'follow',
-      referrerPolicy: 'no-referrer',
-      body: JSON.stringify(data)
+        method: 'POST',
+        mode: 'no-cors',
+        cache: 'no-cache',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify(data)
     });
     return response;
-  }
+}
 
-function queryBackEnd(data){
-    let productInfo = postData(BACK_END_URL, {ASIN : data});
-    console.log(productInfo);
+function queryBackEnd(data) {
+    postData(BACK_END_URL, { ASIN: data })
+        .then(
+            data => {
+                console.log(data.text());
+            }
+        );
 }
 
 let ASIN = getASIN();
