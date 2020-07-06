e04419e6fae3b739437b56399c7f756fb0bf3e9b
Samuel Kent <kent_sam@outlook.com>
2020-07-06 13:33:15 +0100

    Cleaned up, now the request is done from the background script.

--- background.js
+++ background.js
@@ -1,3 +1,49 @@
 // Background handling script.
 
 "use strict";
+
+const BACK_END_URL = "https://recyclabilitydiscriminator.eu-gb.mybluemix.net/amazonproduct"
+//const BACK_END_URL = "https://riviera-amadeus-8000.codio.io/amazonproduct"
+
+
+async function postData(url, data) {
+    let response = await fetch(url, {
+        method: 'POST',
+        cors: 'cors',
+        cache: 'no-cache',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify(data)
+    });
+
+    return response;
+}
+
+function queryBackEnd(data) {
+    postData(BACK_END_URL, { ASIN: data })
+        .then(
+            data => {
+                console.log(data.json());
+            }
+        );
+}
+
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+    for (var key in changes) {
+        var storageChange = changes[key];
+        console.log('Storage key "%s" in namespace "%s" changed. ' +
+            'Old value was "%s", new value is "%s".',
+            key,
+            namespace,
+            storageChange.oldValue,
+            storageChange.newValue);
+
+        if(key == "ASIN"){
+            queryBackEnd(changes[key].newValue);
+        }
+    }
+});
--- product-scan.js
+++ product-scan.js
@@ -3,6 +3,7 @@
 "use strict";
 
 const BACK_END_URL = "https://recyclabilitydiscriminator.eu-gb.mybluemix.net/amazonproduct"
+//const BACK_END_URL = "https://riviera-amadeus-8000.codio.io/amazonproduct"
 
 // Checks for the ASIN on the Amazon page.
 function getASIN() {
@@ -32,7 +33,7 @@ function getASIN() {
     for (let i = 0; i < labels.length; i++) {
         let label = labels[i];
         //console.log(label);
-        if(label.innerHTML == "ASIN"){
+        if (label.innerHTML == "ASIN") {
             let ASINarea = label.parentElement.parentElement.innerHTML;
 
             ASIN = ASINarea.substr(ASINarea.length - 10, ASINarea.length - 1);
@@ -45,28 +46,31 @@ function getASIN() {
 async function postData(url, data) {
     let response = await fetch(url, {
         method: 'POST',
-        mode: 'no-cors',
         cache: 'no-cache',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
         },
         redirect: 'follow',
         referrerPolicy: 'no-referrer',
         body: JSON.stringify(data)
     });
 
-    return response.text();
+    return response;
 }
 
 function queryBackEnd(data) {
     postData(BACK_END_URL, { ASIN: data })
         .then(
             data => {
-                console.log(data);
+                console.log(data.text());
             }
         );
 }
 
 let ASIN = getASIN();
-console.log("Product Identification Number: " + ASIN);
-queryBackEnd(ASIN);
+//console.log("Product Identification Number: " + ASIN);
+
+chrome.storage.sync.set({'ASIN': ASIN }, function () {
+    message('ASIN saved to memory.');
+});
