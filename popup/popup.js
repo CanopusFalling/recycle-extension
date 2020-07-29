8deb0a3fbad7fdd89c0442d80d94b050b605d9a8
Sam Rhodes <sdrhodes1@sheffield.ac.uk>
2020-07-29 21:15:38 +0100

    Needs Info-button being placed better, but functionality & info page added

--- popup/popup.js
+++ popup/popup.js
@@ -1,10 +1,17 @@
 // Script to handle the extension's popup page.
 
 "use strict";
+
+
+
 window.onload = function(){ 
     var explore = document.getElementById('explorealts');
+    var infobutton = document.getElementById('info');
+    var back = document.getElementById('back');
+    document.getElementById('infosection').style.display = "none";
     explore.addEventListener('click', exploreoptions); 
-    
+    back.addEventListener('click',returnhome);
+    infobutton.addEventListener('click', loadinfopage);
 }
 
 
@@ -12,6 +19,16 @@ window.onload = function(){
 // Reads the product information and materials results.
 let exploreoptions = function(){
     openPage("/popup/exploreproducts.html")
+}
+let returnhome = function(){
+    document.getElementById('infosection').style.display = "none";
+    document.getElementById('current-product-rating').style.display = "block";
+}
+let loadinfopage = function(){
+    
+    document.getElementById('infosection').style.display = "block";
+    document.getElementById('current-product-rating').style.display = "none";
+    
 }
 function openPage(page) {
     chrome.tabs.create({url:page});
