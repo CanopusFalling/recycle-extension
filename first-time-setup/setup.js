2943b9d7a26e0853e49e84f0096c9942e86d7b0e
Samuel Kent <kent_sam@outlook.com>
2020-09-26 09:06:55 +0100

    That should be all to fix the setup prompt.

--- first-time-setup/setup.js
+++ first-time-setup/setup.js
@@ -6,17 +6,27 @@
 const SETUP_STORAGE_KEY = "extension_setup_status";
 const SETUP_SITE_LOCATION = "first-time-setup/page/setup-page.html"
 
+// Setup info. 
+// This constant will determine if the extension redoes the setup.
+const setupInfo = 
+{
+    lastSetupChange: "0.1.0"
+};
+
 // ===== Setup Script =====
 function setupExtension() {
     chrome.tabs.create({ "url": SETUP_SITE_LOCATION });
 }
 
+// ===== Version Checker =====
 // Check whether new version is installed.
 chrome.runtime.onInstalled.addListener(function(details){
     if(details.reason == "install"){
         setupExtension();
     }else if(details.reason == "update"){
-        var thisVersion = chrome.runtime.getManifest().version;
-        console.log("Updated from " + details.previousVersion + " to " + thisVersion + "!");
+        // Check if the current version needs the setup running again.
+        if(details.previousVersion <= setupInfo.lastSetupChange){
+            setupExtension();
+        }
     }
 });
