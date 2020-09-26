10b3d57b7133d09733ef5ac163c3e926d4228c0f
Sam Rhodes <sdrhodes1@sheffield.ac.uk>
2020-09-26 16:19:35 +0100

    Added Entry Fields

--- testing_script/test_manager.js
+++ testing_script/test_manager.js
@@ -7,6 +7,10 @@ window.onload = function(){
 }
 function AddIssue(){
     var TestIssueNumber = prompt("Please enter Issue number: ","#01");
+    var IssueDescription = prompt("Please enter a description of the issue: ","Enter Description Here");
+    var RecreationParameters = prompt("Please describe how to recreate issue: ","Issue");
+    var DateDiscovery = prompt("Date Discovered: ","DD/MM/YYYY");
+    
     if (TestIssueNumber == null || TestIssueNumber == "") {
       testnum = "Invalid number";
     } else {
