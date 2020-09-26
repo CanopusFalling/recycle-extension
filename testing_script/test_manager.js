49e6b26d7c21addf15c6a534c53b98528e48eee3
Sam Rhodes <sdrhodes1@sheffield.ac.uk>
2020-09-26 14:54:35 +0100

    Started Developing Inhouse Test recorder

---
+++ testing_script/test_manager.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// ===== On Load Script =====
+// Adds the event listeners for the products on the page.
+window.onload = function(){ 
+    
+}
+function AddIssue(){
+    var TestIssueNumber = prompt("Please enter Issue number: ","#01");
+    if (TestIssueNumber == null || TestIssueNumber == "") {
+      testnum = "Invalid number";
+    } else {
+      testnum = TestIssueNumber;
+    }
+    //document.getElementById("demo").innerHTML = txt;
+  }
+
+
+}
