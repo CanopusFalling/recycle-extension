78d51e640d5b7323d15808717299acb5ba7df1b2
Samuel Kent <kent_sam@outlook.com>
2021-02-06 11:03:37 +0000

    Added information about google analytics.

--- first-time-setup/setup.js
+++ first-time-setup/setup.js
@@ -10,7 +10,7 @@ const SETUP_SITE_LOCATION = "first-time-setup/page/setup-page.html"
 // This constant will determine if the extension redoes the setup.
 const setupInfo = 
 {
-    lastSetupChange: "0.1.0"
+    lastSetupChange: "0.1.1"
 };
 
 // ===== Setup Script =====
